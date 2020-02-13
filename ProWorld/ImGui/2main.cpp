d79af0a7e1a038e29b07150f3f6261eea00a6a96
Josep Huguet <rchuguetgironjosep@gmail.com>
2020-02-13 11:10:41 +0100

    compila a la uni

--- ProWorld/ImGui/2main.cpp
+++ ProWorld/ImGui/2main.cpp
@@ -1,7 +1,7 @@
 // dear imgui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
 // If you are new to dear imgui, see examples/README.txt and documentation at the top of imgui.cpp.
 // (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan graphics context creation, etc.)
-
+/*
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
@@ -191,5 +191,5 @@ int main(int, char**)
     glfwTerminate();
 
     return 0;
-}
+}*/
 
